7ecdaf7c9f69f2961ca95c4ce5e7c9c9c53ac096
blu3fishez <kcw2205@naver.com>
2021-11-23 19:58:41 +0900

    add docs

--- src/model/Stat.ts
+++ src/model/Stat.ts
@@ -2,10 +2,16 @@ import { MCFunction, Objective, tellraw } from "sandstone";
 import { MainModel } from './MainModel';
 
 export class Stat {
-    health = Objective.create('model.health', 'dummy');
-    constructor() {
-        MCFunction('model/stat', () => {
-            tellraw('@a', 'Stat Model Loaded');
-        }, {runOnLoad:true});
-    }
+	heaStat = Objective.create('model.hea', 'dummy');
+	strStat = Objective.create('model.str', 'dummy');
+	hanStat = Objective.create('model.han', 'dummy');
+	dexStat = Objective.create('model.dex', 'dummy');
+	intStat = Objective.create('model.int', 'dummy');
+	lucStat = Objective.create('model.luc', 'dummy');
+
+	constructor() {
+		MCFunction('model/stat', () => {
+			tellraw('@a', 'Stat Model Loaded');
+		}, { runOnLoad: true });
+	}
 }
